28ff4b9a9a07245d689593abbe86fea6422dca5f
SHASHANK BHAT <shashankbhat1800@gmail.com>
2019-07-06 13:17:06 +0530

    all set

--- app/src/main/java/com/shashankbhat/androidnotes/Adapters/ShowPageRecyclerViewAdapter.java
+++ app/src/main/java/com/shashankbhat/androidnotes/Adapters/ShowPageRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.shashankbhat.androidnotes.Adapters;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,10 @@ import com.bumptech.glide.Glide;
 import com.shashankbhat.androidnotes.Objects.ShowPageObject;
 import com.shashankbhat.androidnotes.R;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class ShowPageRecyclerViewAdapter extends RecyclerView.Adapter<ShowPageRecyclerViewAdapter.ViewHolder>{
@@ -47,7 +52,8 @@ public class ShowPageRecyclerViewAdapter extends RecyclerView.Adapter<ShowPageRe
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.titleText.setText(showPageObjects.get(position).getTitleText());
-        holder.content.setText(showPageObjects.get(position).getContentUrl());
+        ReadText readText = new ReadText(holder.content);
+        readText.execute(showPageObjects.get(position).getContentUrl());
         Glide.with(context).load(showPageObjects.get(position).getRawDataUrl()).into(holder.imageView);
     }
 
@@ -55,6 +61,43 @@ public class ShowPageRecyclerViewAdapter extends RecyclerView.Adapter<ShowPageRe
     public int getItemCount() {
         return showPageObjects == null ? 0 : showPageObjects.size();
     }
+}
 
+class ReadText extends AsyncTask<String, Void, String>{
 
+    private TextView textView;
+    ReadText(TextView content) {
+        this.textView = content;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        StringBuilder result = new StringBuilder();
+        URL url;
+        HttpURLConnection httpURLConnection;
+        InputStreamReader inputStreamReader;
+        BufferedReader bufferedReader;
+
+        try {
+            url = new URL(params[0]);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+
+            String line = "";
+            while (line != null){
+                line = bufferedReader.readLine();
+                result.append(line);
+            }
+
+        }catch (Exception ignored){}
+        return result.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        textView.setText(s);
+    }
 }
